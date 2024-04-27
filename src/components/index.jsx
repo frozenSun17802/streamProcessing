86c3b4480430bcd4774c059f39849ba8497a9bf4
frozenSun17802 <frozenSun17802@protonmail.com>
2024-04-27 23:16:08 +0300

    add new styles

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import Sidebar from "./sidebar";
+import Navbar from "./navbar";
+import Feed from "./feed";
+import Rightbar from "./rightbar";
+import Post from "./post";
+import Add from "./add";
+import Form from "./form";
+
+export { Sidebar, Navbar, Rightbar, Feed, Post, Add, Form };
